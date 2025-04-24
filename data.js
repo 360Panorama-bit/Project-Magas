442fc14318c5edd55c97c96429b1c37a1a696234
360Panorama-bit <bosson015@gmail.com>
2025-04-24 06:00:26 +0300

    Upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-zal",
-      "name": "Zal",
+      "id": "0-360_1-",
+      "name": "360_1-Этаж",
       "levels": [
         {
           "tileSize": 256,
@@ -22,66 +22,66 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1536,
+      "faceSize": 1750,
       "initialViewParameters": {
-        "yaw": -0.602597121432936,
-        "pitch": 0.14514273137578648,
-        "fov": 1.3568621924490027
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.6542404104743724,
-          "pitch": 0.24183868134142017,
-          "rotation": 0,
-          "target": "1-kabinet"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-kabinet",
-      "name": "Kabinet",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1536,
-      "initialViewParameters": {
-        "yaw": -0.6171251193801872,
+        "yaw": 0.045980059999982004,
         "pitch": 0,
         "fov": 1.3568621924490027
       },
       "linkHotspots": [
         {
-          "yaw": 2.719920161084284,
-          "pitch": 0.107191693510444,
+          "yaw": -0.02861210556833882,
+          "pitch": -0.17524955187884217,
           "rotation": 0,
-          "target": "0-zal"
+          "target": "1-360_2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360_2-",
+      "name": "360_2-Этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1750,
+      "initialViewParameters": {
+        "yaw": 0.10271758999999747,
+        "pitch": 0,
+        "fov": 1.3568621924490027
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005321570102129414,
+          "pitch": 0.2238214211975027,
+          "rotation": 0,
+          "target": "0-360_1-"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Kvartira360",
+  "name": "360-Panorama",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
